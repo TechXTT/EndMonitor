51cece83051fc6280b4e49ffd48373ed134bc997
School_Bozhinov <marogo142005@gmail.com>
2023-05-30 20:02:12 +0300

    [FIX] Repositories

--- src/main/java/me/bozhilov/EndMonitor/repository/CompanyRepository.java
+++ src/main/java/me/bozhilov/EndMonitor/repository/CompanyRepository.java
@@ -1,11 +1,13 @@
 package me.bozhilov.EndMonitor.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import me.bozhilov.EndMonitor.model.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-    Company findByName(String name);
+    Optional<Company> findByName(String name);
 
     Company findById(long id);
 
--- src/main/java/me/bozhilov/EndMonitor/repository/UserRepository.java
+++ src/main/java/me/bozhilov/EndMonitor/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package me.bozhilov.EndMonitor.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import me.bozhilov.EndMonitor.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
